75b3e9af4c36d72f4c1d552d43c11a2203741797
592357390@qq.com <ym1314.yxf>
2019-06-29 14:10:40 +0800

    我的简历

---
+++ parttimejob/src/main/java/fu/com/parttimejob/activity/MyJianLiActivity.kt
@@ -0,0 +1,19 @@
+package fu.com.parttimejob.activity
+
+import android.os.Bundle
+import fu.com.parttimejob.R
+import fu.com.parttimejob.base.BaseActivity
+
+class MyJianLiActivity : BaseActivity() {
+    override fun getLayoutId(): Int {
+        return R.layout.activity_my_jian_li
+    }
+
+    override fun initViewParams() {
+    }
+
+    override fun initViewClick() {
+    }
+
+
+}
