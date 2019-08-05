0cf8d8e1bac5eae3d5980e06e388ca26040d68d9
592357390@qq.com <ym1314.yxf>
2019-08-05 21:23:46 +0800

    廣告列表bug

--- parttimejob/src/main/java/fu/com/parttimejob/activity/AdListActivity.kt
+++ parttimejob/src/main/java/fu/com/parttimejob/activity/AdListActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.view.View
 import com.heixiu.errand.net.RetrofitFactory
 import com.lljjcoder.citylist.Toast.ToastUtils
 import fu.com.parttimejob.R
@@ -11,6 +12,8 @@ import fu.com.parttimejob.adapter.AdListAdapter
 import fu.com.parttimejob.base.BaseActivity
 import fu.com.parttimejob.base.baseadapter.BaseRecyclerModel
 import fu.com.parttimejob.base.baseadapter.OnItemClickListener
+import fu.com.parttimejob.bean.JobInfoBean
+import fu.com.parttimejob.bean.MAdvertisingBean
 import fu.com.parttimejob.retrofitNet.RxUtils
 import fu.com.parttimejob.utils.SPUtil
 import kotlinx.android.synthetic.main.activity_ad_list.*
@@ -27,14 +30,7 @@ class AdListActivity : BaseActivity() {
     override fun initViewParams() {
         adList.layoutManager = LinearLayoutManager(this)
         adList.adapter = adListAdapter
-        RxUtils.wrapRestCall(RetrofitFactory.getRetrofit().myAdvertisingCampaignList(SPUtil.getString(this,"thirdAccount",""))).subscribe({
-            adListAdapter.clear();
-            adListAdapter.notifyDataSetChanged() ;
-            adListAdapter.addAll(it as List<BaseRecyclerModel>?)
-            adListAdapter.notifyDataSetChanged()
-        }, {
-            ToastUtils.showLongToast(this, it.message.toString())
-        })
+
     }
 
     override fun initViewClick() {
@@ -48,5 +44,25 @@ class AdListActivity : BaseActivity() {
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+        RxUtils.wrapRestCall(RetrofitFactory.getRetrofit().myAdvertisingCampaignList(SPUtil.getString(this,"thirdAccount",""))).subscribe({
+            if (it.size==0){
+                emptyView.visibility= View.VISIBLE
+            }else{
+                emptyView.visibility= View.GONE
+                adListAdapter.data.clear()
+                adListAdapter.addAll(it as List<BaseRecyclerModel>?)
+                adListAdapter.notifyDataSetChanged()
+            }
+        }, {
+            ToastUtils.showLongToast(this, it.message.toString())
+        })
 
+        adListAdapter.setOnItemClickListener { view, t, position ->
+            val intent = Intent(view.context, AdInfoActivity::class.java)
+            intent.putExtra("id", (adListAdapter.data[position] as MAdvertisingBean).id)
+            startActivity(intent)
+        }
+    }
 }
